d5803d7f81352723e174d9903c956cb4440f660a
Kushal Ghosh <108950724+kushaljgec2025@users.noreply.github.com>
2022-10-22 10:54:15 +0530

    Update home.js

--- home.js
+++ home.js
@@ -12,3 +12,13 @@ function opentab(tabname) {
     document.getElementById(tabname).classList.add("active-tab");
    
 }
+function menuBarOpen(){
+    var x = document.getElementById("menu");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+
+    } else {
+      x.style.display = "block";
+      
+    }
+}
